f1dd1db4ca16b68f15edfb97acab0b0522ef8205
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-04-28 20:02:00 -0400

    Conexão com a API - Aplicando o React Hook Form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -7,8 +7,35 @@ import {
   TransactionType,
   TransactionTypeButton,
 } from './styles';
+import * as zod from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const newTransactionFormSchema = zod.object({
+  description: zod.string().min(3, 'Descrição muito curta'),
+  amount: zod.number().positive('O valor não pode ser negativo'),
+  category: zod.string().min(3, 'Categoria muito curta'),
+  type: zod.enum(['income', 'outcome'], {
+    invalid_type_error: 'Tipo de transação inválido',
+  }),
+});
+
+type NewTransactionFormData = zod.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<NewTransactionFormData>({
+    resolver: zodResolver(newTransactionFormSchema),
+  });
+
+  async function handleCreateNewTransaction(data: NewTransactionFormData) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+
   return (
     <Dialog.Portal>
       <Overlay />
@@ -20,23 +47,39 @@ export function NewTransactionModal() {
           <X size={24} />
         </CloseButton>
 
-        <form action=''>
-          <input type='text' placeholder='Descrição' required />
-          <input type='number' placeholder='Preço' required />
-          <input type='text' placeholder='Categoria' required />
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type='text'
+            placeholder='Descrição'
+            required
+            {...register('description')}
+          />
+          <input
+            type='number'
+            placeholder='Preço'
+            required
+            {...register('amount', { valueAsNumber: true })}
+          />
+          <input type='text' placeholder='Categoria' required {...register('category')} />
 
           <TransactionType>
-            <TransactionTypeButton value='income' variant='income'>
+            <TransactionTypeButton value='income' variant='income' {...register('type')}>
               <ArrowCircleUp size={24} />
               Entrada
             </TransactionTypeButton>
-            <TransactionTypeButton value='outcome' variant='outcome'>
+            <TransactionTypeButton
+              value='outcome'
+              variant='outcome'
+              {...register('type')}
+            >
               <ArrowCircleDown size={24} />
               Saída
             </TransactionTypeButton>
           </TransactionType>
 
-          <button type='submit'>Cadastrar</button>
+          <button type='submit' disabled={isSubmitting}>
+            Cadastrar
+          </button>
         </form>
       </Content>
     </Dialog.Portal>
