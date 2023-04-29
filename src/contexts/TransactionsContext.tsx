271ada89fcf58ff74dc5c17b2d583531b2906c35
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-04-28 21:22:49 -0400

    Performance - Aplicando o useMemo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -54,7 +54,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
   useEffect(() => {
     fetchTransactions();
-  }, []);
+  }, [fetchTransactions]);
 
   return (
     <TransactionsContext.Provider
--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,23 +1,26 @@
 import { useContextSelector } from 'use-context-selector';
 import { TransactionsContext } from '../contexts/TransactionsContext';
+import { useMemo } from 'react';
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (ctx) => ctx.transactions);
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.amount;
-        acc.total += transaction.amount;
-      } else {
-        acc.outcome += transaction.amount;
-        acc.total -= transaction.amount;
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.amount;
+          acc.total += transaction.amount;
+        } else {
+          acc.outcome += transaction.amount;
+          acc.total -= transaction.amount;
+        }
 
-      return acc;
-    },
-    { income: 0, outcome: 0, total: 0 }
-  );
+        return acc;
+      },
+      { income: 0, outcome: 0, total: 0 }
+    );
+  }, [transactions]);
 
   return summary;
 }
